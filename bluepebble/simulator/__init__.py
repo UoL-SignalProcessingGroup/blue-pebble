7ff71333975f62b3c620524cce3a3c1543346b4b
Finley Boulton <146852382+fincb@users.noreply.github.com>
2026-03-10 18:05:52 +0000

    add back the depreciated discrete sim

--- bluepebble/simulator/__init__.py
+++ bluepebble/simulator/__init__.py
@@ -1,12 +1,18 @@
 """Simulator package public API."""
 
 from .base import PassiveSonarArraySimulatorBase
-from .continuous import ContinuousPassiveSonarArraySimulator
-from .discrete import DiscretePassiveSonarArraySimulator
 from .sensor_data import PassiveSonarSensorData
+from .discrete import (
+    DiscretePassiveSonarArraySimulator,
+    DepreciatedDiscretePassiveSonarArraySimulator
+)
+from .continuous import (
+    ContinuousPassiveSonarArraySimulator,
+)
 
 __all__ = [
     "ContinuousPassiveSonarArraySimulator",
+    "DepreciatedDiscretePassiveSonarArraySimulator",
     "DiscretePassiveSonarArraySimulator",
     "PassiveSonarArraySimulatorBase",
     "PassiveSonarSensorData",
